962acdfe6927e7b25de2b724f2070c43ee3b59b3
unknown <Piyusht@PIYUSHT-WINDOWS.talentica-all.com>
2017-08-09 19:45:38 +0530

    Enable Testing
    Piyush

--- user-data.php
+++ user-data.php
@@ -8,11 +8,13 @@
 
 function getUserData(){
     $data = new \stdClass;
-    $data->guid = "{BA2B9813-7FE4-4A87-ABFF-33026DD43545}";
-    $data->firstname = "Mark";
-    $data->lastname = "J";
-    $data->userid = "mark";
+    $data->guid = $_GET['guid']? $_GET['guid']:"{BA2B9813-7FE4-4A87-ABFF-33026DD43545}";
+    $data->firstname = $_GET['firstname']?$_GET['firstname']:"Mark";
+    $data->lastname = $_GET['lastname']?$_GET['lastname']:"J";
+    //$data->userid = $_GET['userid']?$_GET['userid']:"mark";
+    $data->userid= "mark";
+    //$data->company = $_GET['company']?$_GET['company']:"Tradeweb";
     $data->company = "Tradeweb";
-    $data->email = "mark10@tradeweb.com";
+    $data->email = $_GET['email']?$_GET['email']:"mark10@tradeweb.com";
     return $data;
 }
